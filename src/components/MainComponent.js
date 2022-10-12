30d6b93de7a5bfdd2017402da1595245ebb8ae5c
HieuTND <hieutndde160581@fpt.edu.vn>
2022-10-12 15:27:27 +0700

    Single Page Applications Part 2

--- src/components/MainComponent.js
+++ src/components/MainComponent.js
@@ -3,6 +3,7 @@ import Home from './HomeComponent';
 import Menu from './MenuComponent';
 import Contact from './ContactComponent';
 import Header from './HeaderComponent';
+import DishDetail from './DishdetailComponent';
 import Footer from './FooterComponent';
 import { COMMENTS } from '../shared/comments';
 import { PROMOTIONS } from '../shared/promotions';
@@ -22,7 +23,6 @@ class Main extends Component {
     };
   }
 
-
   render() {
     const HomePage = () => {
       return(
@@ -34,12 +34,20 @@ class Main extends Component {
       );
     }
 
+    const DishWithId = ({match}) => {
+      return(
+          <DishDetail dish={this.state.dishes.filter((dish) => dish.id === parseInt(match.params.dishId,10))[0]} 
+            comments={this.state.comments.filter((comment) => comment.dishId === parseInt(match.params.dishId,10))} />
+      );
+    };
+
     return (
       <div>
         <Header/>
         <Switch>
             <Route path='/home' component={HomePage} />
             <Route exact path='/menu' component={() => <Menu dishes={this.state.dishes} />} />
+            <Route path='/menu/:dishId' component={DishWithId} />
             <Route exact path='/contactus' component={Contact} />
             <Redirect to="/home" />
         </Switch>
